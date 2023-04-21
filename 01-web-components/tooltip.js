39fe2d4c0c05f76b526ea7b000c4ded5508ed3b2
Marco Antonio Hernández Pérez <xeynus@hotmail.com>
2023-04-20 18:37:45 -0600

    COmponente tooltip terminado

--- 01-web-components/tooltip.js
+++ 01-web-components/tooltip.js
@@ -14,35 +14,84 @@ class Tooltip extends HTMLElement {
                     background-color: #000000;
                     color: #ffffff;
                 }
+
+                :host {
+                    position: relative;
+                    padding-left: 6px;
+                    border-left: 3px solid #00659f;
+                }
+
+                :host(.danger) {
+                    background: var(--color-danger);
+                    color: #ffffff;
+                }
+
+                :host-context(h1) {
+                    margin: 32px 0;
+                    color: var(--color-prim);
+                }
+
+                ::slotted(span) {
+                    border-bottom: 1px solid var(--color-prim);
+                }
             </style>
             <slot>Vacio</slot>
-            <span> (?) </span>
+            <span class="icon">?</span>
         `;
 
-        this._ttContainer;
         this._ttText = 'default';
+        this._ttIcon;
+        this._tooltipVisible = false;
     }
 
     connectedCallback() {
         ( this.hasAttribute('text') ) && (this._ttText = this.getAttribute("text"));
         
-        const ttIcon = this.shadowRoot.querySelector('span');
+        this._ttIcon = this.shadowRoot.querySelector('span');
+        this._ttIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        this._ttIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+        this._render();
+    }
 
-        ttIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-        ttIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    attributeChangedCallback( name, oldValue, newValue ) {
+        console.log( name, oldValue, newValue );
 
-        this.shadowRoot.appendChild(ttIcon);
-        this.style.position = 'relative';
+        if ( oldValue === newValue ) {
+            return;
+        }
+
+        ( name === 'text' ) && (this._ttText = newValue );
+    }
+
+    static get observedAttributes() {
+        return ['text'];
+    }
+
+    disconnectedCallback() {
+        this._ttIcon.removeEventListener('mouseenter', this._showTooltip);
+        this._ttIcon.removeEventListener('mouseleave', this._hideTooltip);
+    }
+
+    _render() {
+        let ttContainer = this.shadowRoot.querySelector('div');
+
+        if ( this._tooltipVisible ) {
+            ttContainer = document.createElement('div');
+            ttContainer.textContent = this._ttText;
+            this.shadowRoot.appendChild(ttContainer);
+        } else {
+            ( ttContainer ) && ( this.shadowRoot.removeChild(ttContainer) );
+        }
     }
 
     _showTooltip() {
-        this._ttContainer = document.createElement('div');
-        this._ttContainer.textContent = this._ttText;
-        this.shadowRoot.appendChild(this._ttContainer);
+        this._tooltipVisible = true;
+        this._render();
     }
 
     _hideTooltip() {
-        this.shadowRoot.removeChild(this._ttContainer);
+        this._tooltipVisible = false;
+        this._render();
     }
 }
 
